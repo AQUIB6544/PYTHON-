af36797c085748d9ed8a8ea361dcf0023aace223
MOHD AQUIB <mohdaquib195@gmail.com>
2025-08-28 12:10:03 +0530

    Delete test program directory

--- test program/coffeeCustomization.py
+++
@@ -1,8 +0,0 @@
-size = input("ENTER YOUR ORDER SIZE : ")
-user = input("DO YOU WANT EXTRA SHOT ? (type true/false):").lower()
-if user == "true":
-    print("your coffee is",size,"size with extra shot")
-elif user == "false":
-    print("your coffee is",size,"without extra shot")
-else:
-    print("we are not that type of coffee 😅")
--- test program/evenorodd.py
+++
@@ -1,9 +0,0 @@
-try:
-   A = float(input("ENTER A NUMBER:-"))
-
-   if(A%2==0):
-    print("NUMBER IS EVEN")
-   else:
-    print("NUMBER IS ODD")
-except ValueError:
-  print("Please enter a valid integer.")
