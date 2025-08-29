0c54aa89158f1faa743cfacf8695ff60e913d75d
AQUIB6544 <mohdaquib195@gmail.com>
2025-08-29 18:16:52 +0530

    TO DO LIST

--- task-03.py
+++ task-03.py
@@ -5,7 +5,7 @@ todo.title("TO DO LIST")
 todo.iconbitmap("D:\AKTU b.tech\internship\playlist_list_6297.ico")
 todo.geometry("600x400")
 todo.resizable(False,False)
-todo.config(bg="gray")
+todo.config(bg="#121212")
 # FUNCTION FOR ADD
 def add():
     task = ent.get()
@@ -22,21 +22,25 @@ def delete():
     pos = lis.curselection()[0]
     lis.delete(pos)
 # CREATE LABEL
-lab01 = Label(todo,text="TO-DO-LIST",font=("Montserrat Black",20,"bold"))
-lab01.place(x=220,y=10,height=30,width=170)
-lab02 = Label(todo,text="ENTER TASK: ",font=("Montserrat Black",12,"bold"))
+lab01 = Label(todo,text="TO-DO-LIST",font=("Arial",20,"bold"),fg="#00c9ff",bg="#121212")
+lab01.place(x=220,y=15,height=30,width=170)
+lab02 = Label(todo,text="ENTER TASK: ",font=("Arial",13,"bold"),fg="#b2ffff",bg="#121212")
 lab02.place(x=20,y=60,height=30,width=120)
 # CREATE ENTRY BOX
-ent = Entry(todo,font=("Montserrat Black",10,"bold"))
+ent = Entry(todo,font=("Montserrat Black",10,"bold"),bg="#1e1e1e",fg="white",insertbackground="white")
 ent.place(x=150,y=60,height=30,width=430)
 # CREATE LIST 
-lis = Listbox(todo,font=("Montserrat Black",10,"bold"))
+lis = Listbox(todo,font=("Montserrat Black",10,"bold"),bg="#1e1e1e",fg="white",
+              selectbackground="#7f00ff",selectforeground="white")
 lis.place(x=20,y=100,height=185,width=560)
 # CREATE BUTTON 
-but_1 = Button(todo,text="ADD",font=("Montserrat Black",10,"bold"),command=add)
+but_1 = Button(todo,text="ADD",font=("Montserrat Black",10,"bold"),command=add,bg="#7f00ff",fg="white",
+               activebackground="#9d4edd",activeforeground="white",relief="flat",bd=0)
 but_1.place(x=50,y=305,height=45,width=80)
-but_2 = Button(todo,text="MARK",font=("Montserrat Black",10,"bold"),command=mark)
+but_2 = Button(todo,text="MARK",font=("Montserrat Black",10,"bold"),command=mark,bg="#7f00ff",fg="white",
+               activebackground="#9d4edd",activeforeground="white",relief="flat",bd=0)
 but_2.place(x=250,y=305,height=45,width=80)
-but_3 = Button(todo,text="DELETE",font=("Montserrat Black",10,"bold"),command=delete)
+but_3 = Button(todo,text="DELETE",font=("Montserrat Black",10,"bold"),command=delete,bg="#7f00ff",fg="white",
+               activebackground="#9d4edd",activeforeground="white",relief="flat",bd=0)
 but_3.place(x=460,y=305,height=45,width=80)
 todo.mainloop()
